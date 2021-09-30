b410a406d5d1e29b6107d629d4a13cdd66547f91
pavel.milanov <pawel.milanov@uandex.ru>
2021-09-30 23:06:23 +0300

    переработан шаблон

--- frontend/web-app/src/views/Office.vue
+++ frontend/web-app/src/views/Office.vue
@@ -3,13 +3,12 @@
         <div class="triangle opage"></div>
         <article class="officepage">
             <router-link to="/accounts">
-                <button class="btn btn-login">
-                    Log in
+                <button class="btn btn-login" @click="check_authenticated()">
+                    {{button_label}}
                 </button>
             </router-link>
             <div class="officepage-main">
                 Здесь будет личный кабинет
-                <!-- <p>{{this.$store.getters.GET_USER}}</p> -->
                 <router-view />
             </div>
         </article>
@@ -23,13 +22,18 @@ export default {
     name: 'Office',
     data() {
         return {
+            is_authenticated: false,
+            button_label: 'Log in',
             
         }
     },
     components: {
     },
     methods: {
-
+        check_authenticated (){
+            this.is_authenticated = true
+            this.button_label = 'Log out'
+        },
     },
 }
 </script>

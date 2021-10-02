925c9937670e416e81b187f6f330b571bf84e4d8
pavel.milanov <pawel.milanov@uandex.ru>
2021-10-02 23:46:04 +0300

    мелкие правки

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',

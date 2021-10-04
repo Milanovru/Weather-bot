a07a30c75f9b855089f59dbdd6e724dc74ab4808
Milanovru <milanov71@icloud.com>
2021-10-04 15:00:25 +0300

    обавлена ссылка в telegram

--- frontend/web-app/src/views/Home.vue
+++ frontend/web-app/src/views/Home.vue
@@ -8,7 +8,7 @@
       </div>
         <div class="main">    
           <button class="btn btn-tg">
-              <a href="">Join telegram</a>
+              <a href="https://t.me/MPWeatherbot" target="_blank">Join telegram</a>
           </button>
           <div class="futter">
             test text

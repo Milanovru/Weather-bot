d7cc74ab91cae3231639ecccc27b86d4f979f191
pavel.milanov <pawel.milanov@uandex.ru>
2021-10-15 20:40:51 +0300

    добавлена функция вывода доп. информации о пользователе

--- backend/server_api/views.py
+++ backend/server_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 from .serializers import SubscriberSerializer
 from .models import Subscriber
 from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class SubscribersViewSet(viewsets.ModelViewSet):
@@ -10,3 +11,24 @@ class SubscribersViewSet(viewsets.ModelViewSet):
     serializer_class = SubscriberSerializer
     queryset = Subscriber.objects.all()
     permission_classes = [AllowAny]
+
+    @action(detail=True)
+    def get_detail_info(self, request, pk=None):
+        try:
+            subscriber = Subscriber.objects.select_related().get(issubscribed__is_subscribed=True)
+            return Response(
+                {'id': subscriber.id,
+                'name': subscriber.name,
+                'data': subscriber.data,
+                'status': 'подписка оформлена'
+                }
+            )
+        except:
+            subscriber = Subscriber.objects.get(id=pk)
+            return Response(
+                {'id': subscriber.id,
+                 'name': subscriber.name,
+                 'data': subscriber.data,
+                 'status': 'нет активной подписки' 
+                }
+            )
